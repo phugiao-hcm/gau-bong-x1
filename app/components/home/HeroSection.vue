f3805768849feff038ae9340e8bbcd04ec1ff381
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-08-13 10:38:45 +0700

    Update

--- app/components/home/HeroSection.vue
+++ app/components/home/HeroSection.vue
@@ -1,28 +1,28 @@
 <template>
-  <section
-    class="relative text-white py-16 sm:py-20 md:py-28 lg:py-40 px-4"
-    style="
-      background-image: url('/images/bg_qt.png');
-      background-size: cover;
-      background-position: center;
-    "
-  >
-    <!-- Overlay làm tối nền -->
-    <div class="absolute inset-0 bg-black/50"></div>
+    <section
+        class="relative text-white py-16 sm:py-20 md:py-28 lg:py-40 px-4"
+        style="
+            background-image: url('images/bg_qt.png');
+            background-size: cover;
+            background-position: center;
+        "
+    >
+        <!-- Overlay làm tối nền -->
+        <div class="absolute inset-0 bg-black/50"></div>
 
-    <!-- Nội dung -->
-    <div class="relative z-10 max-w-6xl mx-auto text-center">
-      <!-- Tiêu đề -->
-      <h2
-        class="text-2xl sm:text-3xl md:text-4xl font-extrabold uppercase leading-snug"
-      >
-        Gấu Bông Cao Cấp – Món Quà Yêu Thương Tại Phước Hòa, Phú Giáo
-      </h2>
-      <div class="w-16 h-1 bg-orange-500 mx-auto mt-3 mb-4"></div>
-      <p class="max-w-xl mx-auto text-sm sm:text-base">
-        Quà tặng hoàn hảo cho người thân, bạn bè và người yêu! 🎁 Đặt hàng ngay
-        hôm nay – Giao tận nơi nhanh chóng!
-      </p>
-    </div>
-  </section>
+        <!-- Nội dung -->
+        <div class="relative z-10 max-w-6xl mx-auto text-center">
+            <!-- Tiêu đề -->
+            <h2
+                class="text-2xl sm:text-3xl md:text-4xl font-extrabold uppercase leading-snug"
+            >
+                Gấu Bông Cao Cấp – Món Quà Yêu Thương Tại Phước Hòa, Phú Giáo
+            </h2>
+            <div class="w-16 h-1 bg-orange-500 mx-auto mt-3 mb-4"></div>
+            <p class="max-w-xl mx-auto text-sm sm:text-base">
+                Quà tặng hoàn hảo cho người thân, bạn bè và người yêu! 🎁 Đặt
+                hàng ngay hôm nay – Giao tận nơi nhanh chóng!
+            </p>
+        </div>
+    </section>
 </template>
